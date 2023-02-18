18a0375b5428980788830757c8706031936f0b18
Dariusz Gulbicki <kontakt@dariuszgulbicki.pl>
2023-02-18 10:52:03 +0100

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "SWAPI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "SWAPI",
